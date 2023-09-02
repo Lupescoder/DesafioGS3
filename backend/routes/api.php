f99e5ab32bdd893f40c9c3437f3d3eb2efd968b3
lupescoder <wesleylupesgmail.com>
2023-09-01 22:56:31 -0300

    Criando a Migration

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::apiResource('/users', UserController::class);
-
+Route::apiResource('/profiles', ProfileController::class);
